88878e721ee5a26aff68eb7e395a80d188c4c84f
Elton Schmoeller <elton.santos@celk.com.br>
2018-12-04 10:33:09 -0200

    0.4

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -1,35 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-selector: 'app-grocery',
-templateUrl: './app.grocery.html',
-styleUrls: ['../assets/css/bootstrap.min.css']
+  selector: 'app-grocery',
+  templateUrl: './app.grocery.html',
+  styleUrls: ['../assets/css/bootstrap.min.css']
 })
 
 export class GroceryComponent {
-task = {
-name: '',
-id: 0
-};
-tasks = [];
+  task = {
+    name: '',
+    id: 0
+  };
+  tasks = [];
 
-onClick() {
-  if(this.task.id == 0) {
-  this.tasks.push({id: (new Date()).getTime(), name: this.task.name, strike: false});
-  }
-  this.task.name = '';
-  this.task.id = 0;
-  }
-  
-  onEdit(item) {
-  this.task = item;
-  }
-  
-  onDelete(item) {
-  this.tasks = this.tasks.filter(i => i.id !== item.id)
+  onClick(){
+    if(this.task.id == 0){
+      this.tasks.push({
+        id: (new Date()).getTime(),
+        name: this.task.name, 
+        strike: false});
+    }
+    
+    this.task = {
+      name: '',
+      id: 0
+    };
   }
   
-  onStrike(item) {
-  this.tasks.filter(i => i.id === item.id)[0].strike = !this.tasks.filter(i => i.id === item.id)[0].strike;
+  onEdit(item){
+    this.task = item;
+  }
+
+  onDelete(item){
+    this.tasks = this.tasks.filter(i => i.id !== item.id)
+  }
+
+  onStrike(item){ 
+    this.tasks.filter(i => i.id === item.id)[0].strike = !this.tasks.filter(i => i.id === item.id)[0].strike;
   }
 }
