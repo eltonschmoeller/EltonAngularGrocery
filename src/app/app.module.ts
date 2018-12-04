e4c6ef81775f649c8eea75a1eaf63c059acaa985
Elton Schmoeller <eltonschmoeller@gmail.com>
2018-12-03 22:46:37 -0200

    0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { GroceryComponent } from './app.grocery';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+GroceryComponent
+],
+imports: [
+BrowserModule,
+FormsModule,
+],
+providers: [],
+bootstrap: [GroceryComponent]
 })
 export class AppModule { }
