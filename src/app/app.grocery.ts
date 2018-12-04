aa646028c9f4562cb7e4879a428545e1edc1c809
Elton Schmoeller <elton.santos@celk.com.br>
2018-12-04 08:54:58 -0200

    0.2

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -13,41 +13,23 @@ id: 0
 };
 tasks = [];
 
-onClick(){
-      if(this.task.id == 0){
-        this.tasks.push({id: (new Date()).getTime(),name: this.task.name, strike: false});
-      }
-
-      this.task = {ng-
-        name: '',
-        id: 0
-      };
-    }
-
-    onEdit(item){
-      this.task = item;
-    }
-
-    onDelete(item){
-      for(var i = 0;i < this.tasks.length; i++){
-        if(item.id == this.tasks[i].id){
-          this.tasks.splice(i,1);
-          break;
-        }
-      }
-    }
-
-    onStrike(item){
-      for(var i = 0;i < this.tasks.length; i++){
-        if(item.id == this.tasks[i].id){
-          if(this.tasks[i].strike){
-            this.tasks[i].strike = false;
-          }
-          else{
-            this.tasks[i].strike = true;
-          }
-          break;
-        }
-      }
-    }
+onClick() {
+  if(this.task.id == 0) {
+  this.tasks.push({id: (new Date()).getTime(), name: this.task.name, strike: false});
+  }
+  this.task.name = '';
+  this.task.id = 0;
+  }
+  
+  onEdit(item) {
+  this.task = item;
+  }
+  
+  onDelete(item) {
+  this.tasks = this.tasks.filter(i => i.id !== item.id)
+  }
+  
+  onStrike(item) {
+  this.tasks.filter(i => i.id === item.id)[0].strike = !this.tasks.filter(i => i.id === item.id)[0].strike;
+  }
 }
